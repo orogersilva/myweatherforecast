1dd7e185e733d6bb9a10fd8b3f81673b699bfeb6
Roger Silva <roger.silva.vortigo@superdigital.com.br>
2022-07-25 12:17:22 -0300

    chore: Added 'ui' platform module.

--- feature/weekly/src/main/java/com/orogersilva/myweatherforecast/weekly/ui/screen/WeeklyForecastSummaryScreen.kt
+++ feature/weekly/src/main/java/com/orogersilva/myweatherforecast/weekly/ui/screen/WeeklyForecastSummaryScreen.kt
@@ -1,6 +1,11 @@
 package com.orogersilva.myweatherforecast.weekly.ui.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.orogersilva.myweatherforecast.weekly.ui.viewmodel.WeeklyForecastSummaryViewModel
 
 @Composable
@@ -8,4 +13,20 @@ fun WeeklyForecastSummaryScreen(
     weeklyForecastSummaryViewModel: WeeklyForecastSummaryViewModel
 ) {
 
+}
+
+@Composable
+fun DayWeatherContent() {
+    
+    Column(
+        modifier = Modifier
+            .size(200.dp, 200.dp)) {
+
+    }
+}
+
+@Preview
+@Composable
+fun DayWeatherContentPreview() {
+    DayWeatherContent()
 }
