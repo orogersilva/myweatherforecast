3f56ace82f7ca9f1987d50c2650500b9c577e25b
Roger Silva <roger.silva.vortigo@superdigital.com.br>
2022-08-27 10:50:23 -0300

    chore: Added generic dialog component.

---
+++ platform/core/ui/src/main/java/com/orogersilva/myweatherforecast/ui/dialog/GenericDialog.kt
@@ -0,0 +1,116 @@
+package com.orogersilva.myweatherforecast.ui.dialog
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.orogersilva.myweatherforecast.ui.theme.PurpleGrey40
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GenericDialog(
+    title: String,
+    description: String,
+    dismissLabel: String,
+    onDismiss: () -> Unit,
+    confirmLabel: String,
+    onConfirm: () -> Unit
+) {
+
+    Card(
+        shape = RoundedCornerShape(10.dp),
+        modifier = Modifier.padding(10.dp),
+        elevation = CardDefaults
+            .cardElevation(8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .background(Color.White)
+                .padding(16.dp)
+        ) {
+            Text(
+                text = title,
+                textAlign = TextAlign.Center,
+                fontSize = 20.sp,
+                color = Color.Black,
+                modifier = Modifier
+                    .padding(5.dp)
+                    .fillMaxWidth(),
+                style = MaterialTheme.typography.labelLarge,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = description,
+                textAlign = TextAlign.Center,
+                fontSize = 16.sp,
+                color = Color.Black,
+                modifier = Modifier
+                    .padding(
+                        top = 10.dp,
+                        start = 25.dp,
+                        end = 25.dp
+                    ),
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            TextButton(
+                onClick = { onDismiss.invoke() }
+            ) {
+                Text(
+                    text = dismissLabel,
+                    fontWeight = FontWeight.Bold,
+                    color = PurpleGrey40,
+                    modifier = Modifier.padding(top = 5.dp, bottom = 5.dp)
+                )
+            }
+            TextButton(
+                onClick = { onConfirm.invoke() }
+            ) {
+                Text(
+                    text = confirmLabel,
+                    fontWeight = FontWeight.ExtraBold,
+                    color = Color.Black,
+                    modifier = Modifier.padding(top = 5.dp, bottom = 5.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun GenericDialogPreview() {
+    GenericDialog(
+        title = "Are you sure?",
+        description = "You must to choose between the two options.",
+        dismissLabel = "No",
+        onDismiss = { },
+        confirmLabel = "Yes",
+        onConfirm = { }
+    )
+}
